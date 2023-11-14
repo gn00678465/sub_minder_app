b446dfb54d43f33f439dd8c3f0d2fcb71542f930
MADAO <gn00678465@gmail.com>
2023-11-14 16:49:32 +0800

    config: Add flavors

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void runWithAppConfig() {
   runApp(const MyApp());
 }
 
---
+++ lib/main_dev.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import './app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runWithAppConfig();
+}
