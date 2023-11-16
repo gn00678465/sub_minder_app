b6b60cb371044f5c821370410feeb3021351d915
MADAO <gn00678465@gmail.com>
2023-11-16 15:45:06 +0800

    fix: 修正 SliverPageLayout border 與 currency model

--- lib/model/currency.g.dart
+++ lib/model/currency.g.dart
@@ -7,12 +7,14 @@ part of 'currency.dart';
 // **************************************************************************
 
 Currency _$CurrencyFromJson(Map<String, dynamic> json) => Currency(
+      id: json['id'] as int?,
       name: json['name'] as String,
       code: json['code'] as String,
       symbol: json['symbol'] as String,
     );
 
 Map<String, dynamic> _$CurrencyToJson(Currency instance) => <String, dynamic>{
+      'id': instance.id,
       'name': instance.name,
       'code': instance.code,
       'symbol': instance.symbol,
